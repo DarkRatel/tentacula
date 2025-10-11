75aab5d7184709be844da74c37ff2bb816235f8b
Julia Musk <holajulia@yandex.ru>
2025-10-11 12:43:50 +0300

    Объявление инстанса запроса контакта из AD.
    (cherry picked from commit d8baf2885d06d7ba54577838fc69b9459d240655)

---
+++ suckers/get_contact.py
@@ -0,0 +1,44 @@
+from typing import Type
+
+from pydantic import BaseModel
+
+from moduls.post_base import create_post
+from sites.suckers import router_sucker
+from ds.ds_dict import DSDict
+from ds.ds_hook import DSHook, DS_TYPE_OBJECT, DS_TYPE_SCOPE
+
+
+class SpecData(BaseModel):
+    login: str
+    password: str
+    host: str
+    port: int = 636
+    base: str = None
+
+    identity: str | Type[DSDict] = None
+    ldap_filter: str = None
+    properties: str | list | tuple = None
+    search_scope: DS_TYPE_SCOPE = "subtree"
+
+
+def get_contact(login: str, password: str, host: str, port: int = 636, base: str = None,
+              identity: str | DSDict = None, ldap_filter: str = None, properties: str | list | tuple = None,
+              search_scope: DS_TYPE_SCOPE = "subtree"):
+    with DSHook(
+            login=login,
+            password=password,
+            host=host,
+            port=port,
+            base=base,
+    ) as ds:
+        result = ds.get_contact(
+            identity=identity,
+            ldap_filter=ldap_filter,
+            properties=properties,
+            search_scope=search_scope,
+        )
+
+    return result
+
+
+create_post("get_contact", SpecData, get_contact, router_sucker)
