66399a063ae3356be9ed17b827799c3d4e0b5423
Julia Musk <holajulia@yandex.ru>
2025-10-11 13:45:21 +0300

    изменение атрибутов в AD установка пароля
    (cherry picked from commit af787c4915f02cd95e25c6c57f8a1b7d2fa51bb9)

---
+++ suckers/set_account_password.py
@@ -0,0 +1,38 @@
+from typing import Type
+from datetime import datetime
+from pydantic import BaseModel
+
+from moduls.post_base import create_post
+from sites.suckers import router_sucker
+from ds.ds_dict import DSDict
+from ds.ds_hook import DSHook, DS_GROUP_SCOPE, DS_GROUP_CATEGORY
+
+
+class SpecData(BaseModel):
+    login: str
+    password: str
+    host: str
+    port: int = 636
+    base: str = None
+
+    identity: str | Type[DSDict]
+    account_password: str
+
+
+def set_account_password(login: str, password: str, host: str, identity: str | DSDict, account_password: str,
+                         port: int = 636, base: str = None):
+    with DSHook(
+            login=login,
+            password=password,
+            host=host,
+            port=port,
+            base=base,
+    ) as ds:
+        ds.set_account_password(
+            identity=identity,
+            account_password=account_password
+
+        )
+
+
+create_post("set_account_password", SpecData, set_account_password, router_sucker)
