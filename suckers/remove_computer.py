6660dc90c2de86bb66cbd4c4d4b5f4c871bd80f1
Julia Musk <holajulia@yandex.ru>
2025-10-11 14:38:22 +0300

    изменение атрибутов в AD удаление компудакхтерочка
    (cherry picked from commit 145f121dbbdc850e0115059d7fb6ce55dc4921be)

---
+++ suckers/remove_computer.py
@@ -0,0 +1,36 @@
+from typing import Type
+from datetime import datetime
+from pydantic import BaseModel
+
+from moduls.post_base import create_post
+from sites.suckers import router_sucker
+from ds.ds_dict import DSDict
+from ds.ds_hook import DSHook
+
+
+class SpecData(BaseModel):
+    login: str
+    password: str
+    host: str
+    port: int = 636
+    base: str = None
+
+    identity: str | DSDict
+
+
+def remove_computer(login: str, password: str, host: str, identity: str | DSDict,
+                  port: int = 636, base: str = None):
+    with DSHook(
+            login=login,
+            password=password,
+            host=host,
+            port=port,
+            base=base,
+    ) as ds:
+        ds.remove_computer(
+            identity=identity
+
+        )
+
+
+create_post("remove_computer", SpecData, remove_computer, router_sucker)
