933e5875228650cafbad451c71893a59417038f9
Julia Musk <holajulia@yandex.ru>
2025-10-11 13:16:42 +0300

    изменение атрибутов группы в AD
    (cherry picked from commit c719eb7826621297285e2d27745e18ce75f0f87c)

---
+++ suckers/set_group.py
@@ -0,0 +1,53 @@
+from typing import Type
+from datetime import datetime
+from pydantic import BaseModel
+
+from moduls.post_base import create_post
+from sites.suckers import router_sucker
+from ds.ds_dict import DSDict
+from ds.ds_hook import DSHook, DS_GROUP_SCOPE, DS_GROUP_CATEGORY
+
+
+class SpecData(BaseModel):
+    login: str
+    password: str
+    host: str
+    port: int = 636
+    base: str = None
+
+    identity: str | Type[DSDict]
+    remove: dict = None
+    add: dict[str, list] = None
+    replace: dict[str, list] = None
+    clear: list[str] = None
+    display_name: str = None
+    group_scope: DS_GROUP_SCOPE = None
+    group_category: DS_GROUP_CATEGORY = None
+
+
+def set_group(login: str, password: str, host: str, identity: str | DSDict, port: int = 636, base: str = None,
+              remove: dict = None,
+              add: dict[str, list] = None,
+              replace: dict[str, list] = None, clear: list[str] = None, display_name: str = None,
+              group_scope: DS_GROUP_SCOPE = None, group_category: DS_GROUP_CATEGORY = None):
+    with DSHook(
+            login=login,
+            password=password,
+            host=host,
+            port=port,
+            base=base,
+    ) as ds:
+        ds.set_group(
+            identity=identity,
+            remove=remove,
+            add=add,
+            replace=replace,
+            clear=clear,
+            display_name=display_name,
+            group_scope=group_scope,
+            group_category=group_category
+
+        )
+
+
+create_post("set_group", SpecData, set_group, router_sucker)
