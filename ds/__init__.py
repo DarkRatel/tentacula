da211a91934c94862907385fff26b8569cdf69a6
Valentin <5valentin5@rambler.ru>
2025-10-24 14:16:04 +0300

    Обновлён список интерпретаторов

---
+++ ds/__init__.py
@@ -0,0 +1,4 @@
+from .ds_hook import DSHook, DS_TYPE_SCOPE, DS_TYPE_OBJECT, DS_GROUP_SCOPE, DS_GROUP_CATEGORY
+from .ds_dict import DSDict
+
+__all__ = ["DSHook", "DSDict", "DS_TYPE_SCOPE", "DS_TYPE_OBJECT", "DS_GROUP_SCOPE", "DS_GROUP_CATEGORY"]
