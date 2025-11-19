559dc438488c9051041c4fce90fdbd37c68e6b2e
Valentin <5valentin5@rambler.ru>
2025-11-19 14:24:10 +0300

    Правка кавычек для совместимости с 3.11

--- app/moduls/auth/auth_manager.py
+++ app/moduls/auth/auth_manager.py
@@ -12,14 +12,14 @@ def current_user(request: Request) -> User | None:
     """
     Механизм авторизации пользователя. Читает headers на наличие корректных атрибутов
     """
-    logger.info(f"Protocol: {request.headers["x-forwarded-proto"].upper()}, "
-                f"Host name: {request.headers["host"]}, "
-                f"Host ip: {request.headers["x-server-ip"]}")
+    logger.info(f"Protocol: {request.headers['x-forwarded-proto'].upper()}, "
+                f"Host name: {request.headers['host']}, "
+                f"Host ip: {request.headers['x-server-ip']}")
 
-    subject = request.headers.get("x-client-subject")
-    serial = request.headers.get("x-client-serial")
+    subject = request.headers.get('x-client-subject')
+    serial = request.headers.get('x-client-serial')
 
-    logger.info(f"Client ip: {request.headers.get("x-forwarded-for")}, "
+    logger.info(f"Client ip: {request.headers.get('x-forwarded-for')}, "
                 f"Client cert Subject: {subject}, Client cert Serial: {serial}")
 
     # Если не были полученные subject и serial сертификата, обработка запроса прерывается
