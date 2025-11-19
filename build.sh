153781087b08dbdde636642c02f364f8da5cafbe
Valentin <5valentin5@rambler.ru>
2025-11-19 14:18:44 +0300

    Корректировка правил создания Conda-Pack с env окружением

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-# Создание окуружения Conda
-conda env create -f environment.yml
+# Удаление предыдущей версии
+conda env remove -n tentacula_env
+
+# Создание окружения Conda
+conda env create -f environment.yml -n tentacula_env
 
 # Активация окружения
-conda activate tentacula
+conda activate tentacula_env
 
+# Отключено, поскольку Conda сохраняет все необходимые зависимости
 # Установка зависимостей pip
-pip install -r app/requirements.txt
+# pip install -r app/requirements.txt
 
 # Упаковка окружения
-conda-pack -n tentacula -o tentacula.tar.gz --compress-level 9
+conda-pack -n tentacula_env -o tentacula_env.tar.gz
