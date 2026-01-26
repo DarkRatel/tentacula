6338f232fbc9ad433ad857176293b2d18f5f82aa
Valentin <5valentin5@rambler.ru>
2026-01-26 14:20:11 +0300

    В шеддулер работы с DS добавлена дешифровка значений

--- app/scheduler/ds.py
+++ app/scheduler/ds.py
@@ -1,15 +1,15 @@
-import base64
 import json
-import time
 import uuid
-from io import StringIO
-import logging
+import base64
+import httpx, ssl
+from functools import reduce
 from datetime import datetime, timezone, timedelta
 
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, ForeignKey, SmallInteger, select, and_, or_, \
-    func, delete
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, ForeignKey, SmallInteger, select, and_, or_, \
+    func, delete
 
 from app.systems.config import AppConfig
 from app.systems.logging import logger, s_id_ctx_var
@@ -35,11 +35,21 @@ class Tasker(Base):
                        onupdate=datetime.now(timezone.utc), server_default=func.now())
 
 
-def decode_param(param):
+def decode_param(param) -> dict:
     param = base64.b64decode(param.encode('utf-8'))
 
-    param = AppConfig.SECRET_KEY.decrypt(
-        param,
+    key_len = int.from_bytes(param[:4], "big")
+    offset = 4
+
+    encrypted_aes_key = param[offset:offset + key_len]
+    offset += key_len
+
+    nonce = param[offset:offset + 12]
+    ciphertext = param[offset + 12:]
+
+    # Расшифровка AES-ключа
+    aes_key = AppConfig.SECRET_KEY.decrypt(
+        encrypted_aes_key,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -47,7 +57,11 @@ def decode_param(param):
         )
     )
 
-    return json.loads(param.decode('utf-8'))
+    # Расшифровка текста
+    aesgcm = AESGCM(aes_key)
+    plaintext = aesgcm.decrypt(nonce, ciphertext, None)
+
+    return json.loads(plaintext.decode('utf-8'))
 
 
 async def run_execution():
@@ -70,8 +84,34 @@ async def run_execution():
                     param_conn = decode_param(task.param_conn)
                     param_query = decode_param(task.param_query)
 
-                    with DSHook(**param_conn) as ds:
-                        result = getattr(ds, type_query)(**param_query)
+                    logger.info(param_conn)
+                    logger.info(param_query)
+
+                    transit = AppConfig.TRANSIT.get(param_conn['host'])
+                    if transit:
+                        logger.info('Transit %s', param_conn['host'])
+
+                        ssl_ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=transit['SSL_CA_CERTS'])
+                        ssl_ctx.load_cert_chain(certfile=transit['SSL_CERTFILE'], keyfile=transit['SSL_KEYFILE'])
+                        transport = httpx.HTTPTransport(verify=ssl_ctx)
+                        timeout = httpx.Timeout(connect=900, read=900, write=900, pool=900)
+                        client = httpx.Client(transport=transport, timeout=timeout)
+
+                        param_conn['host'] = transit['HOST']
+
+                        url = f"{transit['ADDRESS']}/ds/{type_query}"
+                        logger.info("POST: %s", url)
+
+                        response = client.post(
+                            url,
+                            json={**param_conn, **param_conn},
+                        )
+                        response.raise_for_status()
+
+                        result = response.json()
+                    else:
+                        with DSHook(**param_conn) as ds:
+                            result = getattr(ds, type_query)(**param_query)
 
                     status = 'complete'
                 except Exception as e:
