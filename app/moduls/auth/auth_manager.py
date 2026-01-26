c66cbc3b153dda0b8d428045c6dc93647c27dbf9
Valentin <5valentin5@rambler.ru>
2026-01-26 14:17:35 +0300

    В функции авторизации обновлён механизм сертификатов - если включена проверка сертификатов, то проверяется валидность CN на основе LIST_OF_CERTIFICATES

--- app/moduls/auth/auth_manager.py
+++ app/moduls/auth/auth_manager.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, status, Request
 from pydantic import BaseModel
 
+from app.systems.config import AppConfig
 from app.systems.logging import logger
 
 
@@ -17,14 +18,24 @@ def current_user(request: Request) -> User | None:
                 f"Host ip: {request.headers['x-server-ip']}, "
                 f"URL: {request.url}")
 
-    subject = request.headers.get('x-client-subject')
-    serial = request.headers.get('x-client-serial')
+    username = 'Anonymous'
 
-    logger.info(f"Client ip: {request.headers.get('x-forwarded-for')}, "
-                f"Client cert Subject: {subject}, Client cert Serial: {serial}")
+    # Если включена аутентификация пользователя по сертификату
+    if AppConfig.AUTHENTICATION_TYPE == 'CERTIFICATE':
+        subject = request.headers.get('x-client-subject')
+        serial = request.headers.get('x-client-serial')
 
-    # Если не были полученные subject и serial сертификата, обработка запроса прерывается
-    if not all([subject, serial]):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Error client certificate")
+        logger.info(f"Client ip: {request.headers.get('x-forwarded-for')}, "
+                    f"Client cert Subject: '{subject}', Client cert Serial: {serial}")
 
-    return User(username=subject)
+        # Если не были полученные subject и serial сертификата, обработка запроса прерывается
+        if not all([subject, serial]):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Error client certificate")
+
+        # Проверка наличия сертификата в разрешённых
+        if subject not in AppConfig.LIST_OF_CERTIFICATES:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Certificate subject not allowed")
+
+        username = subject
+
+    return User(username=username)
