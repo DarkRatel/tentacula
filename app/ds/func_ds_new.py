70be9d8799738cc908b19840f3b817963b73d939
Valentin <5valentin5@rambler.ru>
2026-05-03 23:32:09 +0300

    Переменная формирования ключей переведена в тпи список и добавлены комментарии

--- app/ds/func_ds_new.py
+++ app/ds/func_ds_new.py
@@ -3,11 +3,10 @@
 
 Обработка атрибутов вынесена за приделы функции.
 Есть существенное отличие обработки специальных параметров (их обработка происходит в не функции)
-Поскольку при создании не требуется сперва получать исходный объект
+Поскольку при создании не требуется сперва получать исходный объект, поэтому есть отличия от изменения объектов
 """
 import ldap
 
-from .ds_dict import DSDict
 from .data import DS_TYPE_OBJECT
 from .convertors_value import convert_object_class, convert_value
 
@@ -31,37 +30,46 @@ def ds_new(connect, _logger, dry_run: bool, type_object: DS_TYPE_OBJECT, path: s
     # Формирование distinguishedName и изоляция специальных символов
     dn = f"CN={ldap.dn.escape_dn_chars(name)},{path}"
 
-    dict_object = DSDict()
+    # Формирование списка и обязательного аттрибута.
     # Получение набора ключей создаваемого объекта на основе короткого имени
-    dict_object['objectClass'] = convert_object_class(name=type_object)
+    list_object = [('objectClass', convert_object_class(name=type_object))]
 
-    dict_object.update({'cn': [name], 'name': [name]})
+    # cn, name и distinguishedName принудительно используют одно имя
+    list_object.extend([('cn', [name]), ('name', [name])])
 
     if display_name:
-        dict_object.update({'displayName': [display_name]})
+        list_object.append(('displayName', [display_name]))
 
+    # Ожидается, что специальные атрибуты (обработанные до передачи в функцию) уже обработаны
     if extend:
-        dict_object.update(**extend)
+        for key, value in extend.items():
+            list_object.append((key, value))
 
+    # Проверка, что дополнительные атрибуты не были переданы через ключи ранее и добавление атрибута в общий список
     if other_attributes:
         for key, value in other_attributes.items():
-            if key in dict_object:
-                raise ValueError(f"Атрибут {key} уже был определён. Удалите его из other_attributes")
-            dict_object.update({key: convert_value(key, value)})
+            for (_, k, _) in list_object:
+                if k.lower() == key.lower():
+                    raise ValueError(f"Атрибут {key} уже был определён. Удалите его из other_attributes")
+            list_object.append((key, convert_value(key, value)))
 
     _logger.info(f"New {type_object}: DN: {dn}, "
-                 f"value: { {k: ['***'] if k.lower() == 'unicodepwd' else v for k, v in dict_object.items()} }")
+                 f"value: {[(k, ['***'] if k.lower() == 'unicodepwd' else v) for k, v in list_object]}")
 
-    for key, values in dict_object.items():
+    # Перебор всех значений для проверки являются ли они списком и конвертация в UTF
+    for index, (key, values) in enumerate(list_object):
+        if not isinstance(values, list):
+            raise ValueError(f"In {key} not list")
+
         if key.lower() == 'unicodePwd'.lower():
-            dict_object[key] = [f'"{v}"'.encode("utf-16-le") for v in values]
+            list_object[index] = (key, [f'"{v}"'.encode("utf-16-le") for v in values])
         else:
-            dict_object[key] = [v.encode("utf-8") for v in values]
+            list_object[index] = (key, [v.encode("utf-8") for v in values])
 
     _logger.debug(f"New {type_object}: DN: {dn}, "
-                  f"value: { {k: ['***'] if k.lower() == 'unicodepwd' else v for k, v in dict_object.items()} }")
+                  f"value: {[(k, ['***'] if k.lower() == 'unicodepwd' else v) for k, v in list_object]}")
 
     if not dry_run:
-        connect.add_s(dn, [(key, value) for key, value in dict_object.items()])
+        connect.add_s(dn, list_object)
     else:
         _logger.warning("Enabled dry run")
