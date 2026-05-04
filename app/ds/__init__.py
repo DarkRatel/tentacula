ff371249468eacf97a486e1157c52db74d5e229c
Valentin <5valentin5@rambler.ru>
2026-05-04 11:58:11 +0300

    Правка формирования

--- app/ds/__init__.py
+++ app/ds/__init__.py
@@ -3,7 +3,6 @@
 
 Классы и функции, которые рекомендуется использовать перечисленны далее
 """
-
 from .ds_hook import DSHook, DS_TYPE_SCOPE, DS_TYPE_OBJECT, DS_GROUP_SCOPE, DS_GROUP_CATEGORY
 from .ds_dict import DSDict
 
