a39763a74f8ee51977fce1a80370a139acb0f7cb
Valentin <5valentin5@rambler.ru>
2026-05-04 16:40:26 +0300

    Изменён механизм исполнения заданий из таблицы - теперь на каждое задание создаётся отдельный поток обработки выведенный за приделы основного шедуллера

--- app/scheduler/ds.py
+++ app/scheduler/ds.py
@@ -1,22 +1,41 @@
 import json
 import uuid
 import base64
-import httpx, ssl
-from functools import reduce
 from datetime import datetime, timezone, timedelta
 
+import asyncio
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, ForeignKey, SmallInteger, select, and_, or_, \
-    func, delete
+from sqlalchemy import Column, Integer, String, DateTime, JSON, select, func, delete
 
+from app.sds import SDSHook
 from app.systems.config import AppConfig
 from app.systems.logging import logger, s_id_ctx_var
 from app.systems.database import get_db, Base
-from app.ds import DSHook
-
+from app.moduls.json_encoder import json_encoder
+from app.systems.database import AsyncSessionLocal
 from app.main import scheduler
+
+# Переменная для отслеживания заданий работающих в фоновом режиме
+background_tasks: set[asyncio.Task] = set()
+
+
+def track_background_task(task: asyncio.Task) -> None:
+    """Отслеживания задания"""
+    background_tasks.add(task)
+
+    def _done_callback(t: asyncio.Task):
+        background_tasks.discard(t)
+
+        try:
+            t.result()
+        except asyncio.CancelledError:
+            logger.warning("Background task was cancelled")
+        except Exception:
+            logger.exception("Background task failed")
+
+    task.add_done_callback(_done_callback)
 
 
 # Шаблон таблицы
@@ -65,78 +84,123 @@ def decode_param(param) -> dict:
     return json.loads(plaintext.decode('utf-8'))
 
 
-async def run_execution():
+def run_ds(uuid_session, type_query, param_conn, param_query):
+    """Функция исполнения запроса к СК"""
+    s_id_ctx_var.set(uuid_session)
+    with SDSHook(**param_conn) as ds:
+        result = getattr(ds, type_query)(**param_query)
+        return result
+
+
+async def task_processing(source_uuid: str, task_id: int):
+    """Функция исполнения выбранного задания. В ID события добавляется ID строки из таблицы заданий"""
+    uuid_session = f"{source_uuid}-{task_id}"
+    s_id_ctx_var.set(uuid_session)
+
+    logger.info(f"====Start task id: %s====", task_id)
+
+    # Подключение к БД
+    async with AsyncSessionLocal() as db:
+        try:
+            task = (
+                await db.execute(select(Tasker).where(Tasker.id == task_id))
+            ).scalar_one()
+            task.status = "working"
+            await db.commit()
+
+            type_query = str(task.type_query)
+            param_conn = decode_param(task.param_conn)
+            param_query = decode_param(task.param_query)
+
+            logger.info('Original Query: %s, Param Connect: %s, Param Query: %s',
+                        type_query, param_conn, param_query)
+
+            # Изменение входных параметров, если такое описано в TRANSIT
+            # Данные пришедшие в задание из таблицы более приоритетные
+            if param_conn['host'] in AppConfig.SCHEDULERS_DS__TRANSIT:
+                transit = AppConfig.SCHEDULERS_DS__TRANSIT[param_conn['host']]
+                param_conn['host'] = transit['host']
+                param_conn = transit | param_conn
+
+                logger.info('Transit Param Connect: %s', param_conn)
+
+            # Исполнение запроса
+            result = await asyncio.to_thread(
+                run_ds,
+                uuid_session, type_query, param_conn, param_query
+            )
+
+            task.result = json_encoder(result)
+            task.status = 'complete'
+            await db.commit()
+        except Exception as e:
+            e = str(e)
+            logger.info(f"Error : %s", e)
+
+            async with AsyncSessionLocal() as db:
+                task = (
+                    await db.execute(select(Tasker).where(Tasker.id == task_id))
+                ).scalar_one()
+
+                task.status = 'error'
+                task.result = e
+                await db.commit()
+
+            logger.info(f"====End====")
+
+            raise
+
+        logger.info(f"====End====")
+
+
+async def scheduler_ds_tasker():
+    """Функция поиска заданий в таблице и создания отдельного события для каждого задания"""
+    # Создание строки сессии
+    source_uuid = str(uuid.uuid4())
+    s_id_ctx_var.set(source_uuid)
+
+    logger.info("Active background tasks: %s", len(background_tasks))
+
+    async with AsyncSessionLocal() as db:
+        # Запрос всех заданий в статусе ожидающих обработку
+        result = await db.execute(select(Tasker).where(
+            (Tasker.status == 'waiting')
+        ))
+        tasks = result.scalars().all()
+
+        if not tasks:
+            logger.info("Not found new tasks in DB")
+            return
+
+        # Изменение статуса для взрытых в работу
+        for task in tasks:
+            task.status = "in_line"
+        await db.commit()
+
+        task_ids = [task.id for task in tasks]
+
+        logger.info(f"New tasks id for run: %s", task_ids)
+
+    # Для всех найденных заданий формирование задания для исполнения на заднем фоне
+    for task_id in task_ids:
+        bg_task = asyncio.create_task(task_processing(source_uuid, task_id))
+        track_background_task(bg_task)
+
+
+# Запуск заданий для обращения к СК
+scheduler.add_job(scheduler_ds_tasker, "interval", minutes=1, id="scheduler_ds_tasker",
+                  max_instances=1, coalesce=True)
+
+
+async def scheduler_ds_cleaning():
+    """Функция очистки таблицы от неактуальных заданий"""
     # Создание строки сессии
     s_id_ctx_var.set(str(uuid.uuid4()))
 
-    async for db in get_db():
-        while True:
-            result = await db.execute(select(Tasker).where(
-                (Tasker.status == 'waiting')
-            ))
-            task = result.scalars().first()
-
-            status = None
-            result = None
-
-            if task:
-                try:
-                    type_query = str(task.type_query)
-                    param_conn = decode_param(task.param_conn)
-                    param_query = decode_param(task.param_query)
-
-                    logger.info(param_conn)
-                    logger.info(param_query)
-
-                    transit = AppConfig.TRANSIT.get(param_conn['host'])
-                    if transit:
-                        logger.info('Transit %s', param_conn['host'])
-
-                        ssl_ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=transit['SSL_CA_CERTS'])
-                        ssl_ctx.load_cert_chain(certfile=transit['SSL_CERTFILE'], keyfile=transit['SSL_KEYFILE'])
-                        transport = httpx.HTTPTransport(verify=ssl_ctx)
-                        timeout = httpx.Timeout(connect=900, read=900, write=900, pool=900)
-                        client = httpx.Client(transport=transport, timeout=timeout)
-
-                        param_conn['host'] = transit['HOST']
-
-                        url = f"{transit['ADDRESS']}/ds/{type_query}"
-                        logger.info("POST: %s", url)
-
-                        response = client.post(
-                            url,
-                            json={**param_conn, **param_conn},
-                        )
-                        response.raise_for_status()
-
-                        result = response.json()
-                    else:
-                        with DSHook(**param_conn) as ds:
-                            result = getattr(ds, type_query)(**param_query)
-
-                    status = 'complete'
-                except Exception as e:
-                    status = 'error'
-                    result = {'error': str(e)}
-                finally:
-                    task.status = status
-                    task.result = result
-                    await db.commit()
-            else:
-                logger.info("NOT FOUND QUERY")
-                break
-
-
-scheduler.add_job(run_execution, "interval", minutes=1, id="ds_tasker")
-
-
-async def run_cleaning():
-    # Создание строки сессии
-    s_id_ctx_var.set(str(uuid.uuid4()))
-
-    async for db in get_db():
+    async with AsyncSessionLocal() as db:
         await db.execute(delete(Tasker).where(Tasker.created_at < func.now() - timedelta(hours=1)))
         await db.commit()
 
-
-scheduler.add_job(run_cleaning, "interval", minutes=60, id="ds_tasker_cleaning")
+# Запуск удаления неиспользованных заданий
+scheduler.add_job(scheduler_ds_cleaning, "interval", minutes=60, id="scheduler_ds_cleaning",
+                  max_instances=1, coalesce=True)
